97e0be710df1c260669697c463f36e8f9b4fba1b
leoraggy <leoragual35@gmail.com>
2025-11-01 19:28:32 -0700

    added admin, backend array

--- app.js
+++ app.js
@@ -4,10 +4,31 @@ const app = express();
 
 const PORT = 3004;
 
+// set EJS as our view engine
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+// allows the app to parse form data (req.body)
+app.use(express.urlencoded({ extended: true }));
+
+const contacts = [];
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(`${import.meta.dirname}/views/home.html`);
+  res.render("home");
+});
+
+app.post("/submit_form", (req, res) => {
+  const contact = req.body;
+  contacts.push(contact);
+  // res.render("confirm");
+  res.json(contact);
+});
+
+app.get("/admin", (req, res) => {
+  res.render("admin", { contacts });
 });
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
